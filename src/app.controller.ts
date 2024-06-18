520502bcd8f8bb006a61ade8aac7f5966789f1b4
Héctor Méndez G <hector.mendez.gutierrez@outlook.com>
2024-06-18 11:06:51 -0400

    Creacion de nuevos templates y metodos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,4 +20,48 @@ export class AppController {
     await this.mailService.sendCalendarInvite();
     return "Calendar invite sent";
   }
+
+  @Get('send-survey')
+  async sendThankYouEmail() {
+    await this.mailService.sendThankYouEmail('Héctor Méndez', 'hector.mendez.gutierrez@outlook.com');
+    return 'Thank you email sent';
+  }
+
+  @Get('send-event-invite')
+  async sendEventInvite() {
+    const context = {
+      name: 'Hector Mendez',
+      parkName: 'Parque Natural Aguas de Ramón',
+      trailName: 'Canto del Agua',
+      date: '22/06/2024',
+      startTime: '08:00',
+      endTime: '14:00',
+      visitorCount: 32,
+      subject: 'Deportivo'
+    };
+    await this.mailService.sendEventInvite('hector.mendez.gutierrez@outlook.com', context);
+    return 'Event invite email sent';
+  }
+
+  @Get('send-combined-invite')
+  async sendCombinedInvite() {
+    const emails = [
+      'hector.mendez.gutierrez@outlook.com',
+      'hectortecno2014@gmail.com',
+      'hmendez@solnet.cl'
+    ];
+    const context = {
+      name: 'Hector Mendez',
+      parkName: 'Parque Natural Aguas de Ramón',
+      trailName: 'Canto del Agua',
+      date: '22/06/2024',
+      startTime: '08:00',
+      endTime: '14:00',
+      visitorCount: 32,
+      subject: 'Deportivo'
+    };
+    await this.mailService.sendCombinedInvite(emails, context);
+    return 'Combined invite email sent';
+  }
+
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,9 +1,15 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { createGoogleCalendarEvent } from "./calendar-event";
+import { MailerService } from "@nestjs-modules/mailer";
+import { join } from "path";
+
 
 @Injectable()
 export class MailService {
   private readonly logger = new Logger("MailService");
+
+  constructor(private mailerService: MailerService) {
+  }
 
   async sendCalendarInvite() {
     try {
@@ -11,6 +17,73 @@ export class MailService {
       this.logger.log("Calendar invite sent with link: " + eventLink);
     } catch (error) {
       this.logger.error("Error sending calendar invite", error);
+    }
+  }
+
+  async sendEventInvite(email: string, context: any) {
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        from: "\"Equipo de Parques\" <no-reply@parquecordillera.cl>",
+        subject: "Invitación a Evento",
+        template: "event-invite",
+        context: context,
+        attachments: [
+          {
+            filename: "logo.png",
+            path: join(__dirname, "..", "..", "assets", "logo-correo.png"),
+            cid: "logo"
+          }
+        ]
+      });
+
+      this.logger.log("Event invite email sent");
+    } catch (error) {
+      this.logger.error("Error sending event invite email", error);
+    }
+  }
+
+  async sendThankYouEmail(responsibleName: string, email: string) {
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        from: "\"Parque Cordillera\" <no-reply@parquecordillera.cl>",
+        subject: "Agradecimiento por su Visita",
+        template: "encuesta",
+        context: {
+          responsibleName: responsibleName
+        },
+        attachments: [
+          {
+            filename: "logo.png",
+            path: join(__dirname, "..", "..", "assets", "logo-correo.png"),
+            cid: "logo"
+          }
+        ]
+      });
+
+      this.logger.log("Thank you email sent");
+    } catch (error) {
+      this.logger.error("Error sending thank you email", error);
+    }
+  }
+
+  async sendCombinedInvite(emails: string[], context: any) {
+    try {
+      // Enviar la invitación al calendario
+      const eventLink = await this.sendCalendarInvite();
+
+      // Añadir el enlace del evento al contexto
+      context.eventLink = eventLink;
+
+      // Enviar el correo con la información del evento a todos los destinatarios
+      for (const email of emails) {
+        await this.sendEventInvite(email, context);
+      }
+
+      this.logger.log("Combined invite sent to all recipients");
+    } catch (error) {
+      this.logger.error("Error sending combined invite", error);
     }
   }
 }
